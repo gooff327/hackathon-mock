8d62171889cb3a93f5765eb310ad5c30458b87a5
gouwanfu <gouwanfu@kezaihui.com>
2020-10-30 14:07:16 +0800

    fix reply

--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -83,7 +83,7 @@ export default gql`
         author: User!
         type: CommentTarget!
         content: String!
-        createAt: String !@formatDate
+        createdAt: String !@formatDate
         to: User
     }
 
